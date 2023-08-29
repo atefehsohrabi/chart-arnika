d0e9d4c1b940eaa8601fba59f611ff53077ddcb0
atefehsohrabi <50141900+atefehsohrabi@users.noreply.github.com>
2023-08-29 01:37:42 -0700

    create moda and pdf compponent

---
+++ src/DownloadPdf.js
@@ -0,0 +1,24 @@
+// import React from 'react';
+
+// import Pdf from 'react pdf'
+
+// const ref = React.createRef();
+
+// const PDF = (props) =>{
+//   return(
+//     <>
+//     <div ref={ref}>
+
+//     </div>
+//     <Pdf targetRef  = {ref} filename ="post.pdf">
+//     {({toPdf}) => <button onClick={toPdf}> Capture as PDF</button>}
+
+//     </Pdf>
+//     </>
+//   )
+// }
+
+
+
+
+export default PDF
